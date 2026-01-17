efa641f47a5cdf4e32076aa4b852236aea8e2a6f
tooclarity-Gowtham <tooclarityad05@gmail.com>
2026-01-17 05:35:41 +0530

    Added logo

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="min-h-screen flex items-center justify-center">
-      
+    <main className="min-h-screen flex items-center justify-center gap-6">
+
+      <Image
+        src="/oxbit.png"
+        alt="Oxbit Logo"
+        width={120}
+        height={120}
+        className="rounded-xl"
+        priority
+      />
+
       <h1 className="text-4xl font-bold">
         Oxbit is coming soon!
       </h1>
