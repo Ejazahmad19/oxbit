dda57a15a7dac7a3ff76a7a2af93926950800357
tooclarity-Gowtham <tooclarityad05@gmail.com>
2026-01-17 17:10:30 +0530

    Material colors added

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Space_Grotesk } from "next/font/google";
+
+import Header from "./components/header";
 import "./globals.css";
 
 const spaceGrotesk = Space_Grotesk({
@@ -20,9 +22,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+      </head>
       <body className={spaceGrotesk.className}>
-        {children}
-      </body>
+        <Header />
+        {children}</body>
     </html>
   );
 }
